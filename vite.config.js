545d6189d87791363049188a2bbc3b3c4d92cdfa
student sumantpatel37819 <student pk645245@gmail.com>
2026-03-19 18:06:01 +0530

    host'

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/repo-name/",
   plugins: [react()],
   server: {
     port: 3000,
